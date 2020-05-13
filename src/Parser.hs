a31d10d6c595da0ef3dde456d1fdf45d915c2704
Hirochika Matsumoto <matsujika@gmail.com>
2020-05-13 21:22:20 +0900

    feat(parser.hs): implement parser

---
+++ src/Parser.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Parser where
+
+import           Data.Void
+import           Data.Text                      ( Text )
+
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer    as L
+
+import           Data.Functor                   ( void )
+
+import           Instruction
+
+type Parser = Parsec Void Text
+
+program :: Parser [Instruction]
+program = lexeme $ many instruction
+
+instruction :: Parser Instruction
+instruction = choice
+    [ IncrementPtr <$ symbol ">"
+    , DecrementPtr <$ symbol "<"
+    , IncrementVal <$ symbol "+"
+    , DecrementVal <$ symbol "-"
+    , PutChar <$ symbol "."
+    , GetChar <$ symbol ","
+    , Loop <$> between (symbol "[") (symbol "]") program
+    ]
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
+
+sc :: Parser ()
+sc = L.space (void $ noneOf ("><+-.,[]" :: String)) empty empty
+
